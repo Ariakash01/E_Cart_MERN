d66f3dfa2a4b77bdb3fa660ae0ef95588245913e
Ariakash01 <140905992+Ariakash01@users.noreply.github.com>
2024-12-29 12:35:18 +0530

    Update Footer.js

--- frontend/src/components/layouts/Footer.js
+++ frontend/src/components/layouts/Footer.js
@@ -2,7 +2,7 @@ export  default function Footer (){
     return (
         <footer className="py-1">
             <p className="text-center text-dark mt-1">
-                AKS CART - 2022-2023, All Rights Reserved
+                GANAPATHI CART - 2022-2023, All Rights Reserved
             </p>
         </footer>
     )
